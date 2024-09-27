a4aa2b14f0554f3c579c4bcbc6b2a3f22209661d
Mr.phyle <163591079+Mrphyle@users.noreply.github.com>
2024-09-27 14:36:32 -0300

    Update modularização.py

--- Meuscodigosdomes8ao12/Exel manipulation/modularização.py
+++ Meuscodigosdomes8ao12/Exel manipulation/modularização.py
@@ -1,7 +1,20 @@
+#Sum
 def Sum(n1,n2):
-    print(f"n1=a?{n1==a}")
-    print(f"n2=b?{n2==b}")
     return n1+n2
-a= int(input("Type first number: "))
-b= int(input("Type last number: "))
-print(Sum(a,b))
+#subtract
+def sub(n1,n2):
+    return n1-n2
+#Division
+def div(n1,n2):
+    return n1/n2
+#Multiplication
+def mult(n1,n2):
+    return n1*n2
+def main():
+    a= int(input("Type first number: "))
+    b= int(input("Type last number: "))
+    print((f"sum {Sum(a,b)}"))
+    print((f"subtract {sub(a,b)}"))
+    print((f"divided {div(a,b)}"))
+    print((f"multiply {mult(a,b)}"))
+main()
