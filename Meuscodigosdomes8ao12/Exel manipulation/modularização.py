3c005ca3c0e3c1980003875c1223cb4821d81914
Mr.phyle <163591079+Mrphyle@users.noreply.github.com>
2024-09-27 13:49:09 -0300

    From 1:00Pm to 2:40pm

--- Meuscodigosdomes8ao12/Exel manipulation/modularização.py
+++ Meuscodigosdomes8ao12/Exel manipulation/modularização.py
@@ -1,4 +1,7 @@
 def Sum(n1,n2):
-
-    return sum(n1,n2)
-print(Sum(10,5))
+    print(f"n1=a?{n1==a}")
+    print(f"n2=b?{n2==b}")
+    return n1+n2
+a= int(input("Type first number: "))
+b= int(input("Type last number: "))
+print(Sum(a,b))
