be6306132efcac9a1e2cac4eff1fbd85b976e075
Mr.phyle <163591079+Mrphyle@users.noreply.github.com>
2024-10-11 10:45:19 -0300

    morning in vscode

---
+++ Translator.py
@@ -0,0 +1,33 @@
+#pip install googletrans
+#pip install googletrans==4.0.0-rc1
+def repeatranslatorcode():
+    from googletrans import Translator
+    firstlanguage= input("type the phrase or word to translate: ")
+    languagetotranlate= input("type the lenguage that you want to tranlate: ")
+    def translator_language():
+        if languagetotranlate.lower() == "english":
+            def translate_eng():
+                translator = Translator()
+                translator.translate(firstlanguage)#ex:"Ola mundo"
+                print(f"\noriginal language: {firstlanguage}\n")
+                translated = translator.translate(firstlanguage, dest="en")
+                print(f"translate to english: {translated.text}")
+            translate_eng()
+        elif languagetotranlate.lower() == "portuguese" or "ptbr":
+            def translate_ptbr():
+                translator = Translator()
+                translator.translate(firstlanguage)#ex:"Ola mundo"
+                print(f"\noriginal language: {firstlanguage}\n")
+                translated = translator.translate(firstlanguage, dest="pt")
+                print(f"translate to ptbr: {translated.text}")
+            translate_ptbr()
+    translator_language()
+while True:
+    repeatranslatorcode()
+    repeatcode = input("\n-------------\nyou want translate other phrase(y/n)? ")
+    if repeatcode.lower() == "y":
+        print(f"{"\n"*2}-------\n")
+        pass
+    elif repeatcode.lower() =="n":
+        print("\n code finished!\n---------")
+        break
