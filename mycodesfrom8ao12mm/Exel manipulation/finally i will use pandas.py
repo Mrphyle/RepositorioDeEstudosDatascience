6ee4bffcae1435ca6485e51c43a8aec4144cf611
Mr.phyle <163591079+Mrphyle@users.noreply.github.com>
2024-10-25 14:20:46 -0300

    up

--- mycodesfrom8ao12mm/Exel manipulation/finally i will use pandas.py
+++ mycodesfrom8ao12mm/Exel manipulation/finally i will use pandas.py
@@ -1,12 +1,20 @@
 import pandas as pd 
 import sqlite3 as sql
+import mysql.connector as msc
+from sqlalchemy import create_engine
 #
 def xls_to_sql(exel_file,sheet_name,db_name,table_name):
-    df = pd.read_excel(exel_file,sheet_name=sheet_name)
-    conn = sql.connect(db_name)
-    df.to_sql(table_name, conn, if_exists="replace",index=False)
-    conn.close
-    print(f"Crated table {table_name} in {db_name}.")
+    try:
+        df = pd.read_excel(exel_file,sheet_name=sheet_name)
+        df.to_sql(table_name, conn, if_exists="replace",index=False)
+        conn = msc.connect(
+            host="localhost",
+            user="root",
+            password="",
+            databese="pandinha")
+        index = pd.read_excel("bdexel.xlsx",sheet_name="Planilha1")
+    except:
+        print("ERROR!!!")
 
 file = "bdexcel.xlsx"
 sheet = "Planilha1"
--- mycodesfrom8ao12mm/Exel manipulation/pandas ;).py
+++ mycodesfrom8ao12mm/Exel manipulation/pandas ;).py
@@ -1,7 +1,8 @@
-import pandas as pd
-import mysql.connector as msc
 #CREATE TABLE funcionarios(id int, nome varchar(70), cidade varchar(70), datanacimento varchar(15));
 #resolvesql or sonvesql
+import pandas as pd
+import mysql.connector as msc
+from sqlalchemy import crea
 try:
     conn = msc.connect(
         host="localhost",
@@ -10,3 +11,5 @@ try:
         databese="pandinha"
     )
     index = pd.read_excel("bdexel.xlsx",sheet_name="Planilha1")
+except:
+    print("ERROR!!!")
