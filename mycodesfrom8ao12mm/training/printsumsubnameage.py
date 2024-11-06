156797d7acbdcad071c86139943bfab8eda7a6cf
Diego Simão Gonçalves <163591079+Mrphyle@users.noreply.github.com>
2024-11-06 09:20:58 -0300

    Create printsumsubnameage.py

---
+++ mycodesfrom8ao12mm/training/printsumsubnameage.py
@@ -0,0 +1,13 @@
+#A.
+print("Ola, estou aprendendo Python")
+#B.
+nome,idade = input("qual seu nome? "),input("\nqual sua idade? ")
+print(f"\nSeu nome é {nome} e você tem {idade} anos de idade?")
+
+#C.
+num1 = int(input("digite um numero: "))
+num2 = int(input("Digite um segundo numero:"))
+result_sum = num1 + num2
+print(f"{num1} + {num2} = {result_sum}")
+result_sub = num1 - num2
+print(f"{num1} - {num2} = {result_sub}")
