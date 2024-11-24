08be3f2e5cfc657ba48818e922c5110a232d7954
Mrphyle <diegosimaogoncalves18@gmail.com>
2024-11-24 13:34:15 -0300

    "Updated sleep times and added space to mysql command in grantcomand.py"

---
+++ Mycodesfrom1ao7mm/tentandofazeralgo/grantcomand.py
@@ -0,0 +1,18 @@
+"""def arr(numbes):
+    for i in range(numbes):
+        NumbersToCount = int(input("digite o numero que deseja: "))       
+number_count = int(input("quantos numeros você fara a media? "))
+media = arr(number_count)"""
+import pyautogui as pyauto
+import time
+pyauto.hotkey("ctrl","alt","t")
+time.sleep(1.5)
+pyauto.write("cd /opt/lampp/bin")
+time.sleep(1.5)
+pyauto.hotkey("enter")
+time.sleep(1.5)
+pyauto.write("./mysql -u root -p")
+time.sleep(0.2)
+pyauto.hotkey("enter")
+time.sleep(0.5)
+pyauto.hotkey("enter")
