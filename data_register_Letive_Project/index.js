db41edd270424ca15d4166d26b3cb6f1934bda38
Diego Simão Gonçalves <163591079+Mrphyle@users.noreply.github.com>
2025-04-15 17:39:22 -0300

    Add form fields for toy donation and create Nota_Fiscal table

---
+++ data_register_Letive_Project/index.js
@@ -0,0 +1,7 @@
+const instituitiond_donor = document.getElementById("nomeInstituicao").value;
+const QuantityOfToys = ;
+const LocationReceived = ;
+const DateReceived = ;
+const LocationDonated = ;
+const DateDonated = ;
+const AnyObservations = ;
