ea000ca36c4780a919e7ac79ab1fbcf43b35eb1e
Diego Simão Gonçalves <163591079+Mrphyle@users.noreply.github.com>
2025-04-15 18:13:19 -0300

    Add JavaScript to capture and log toy donation form data

--- data_register_Letive_Project/index.js
+++
@@ -1,7 +0,0 @@
-const instituitiond_donor = document.getElementById("nomeInstituicao").value;
-const QuantityOfToys = ;
-const LocationReceived = ;
-const DateReceived = ;
-const LocationDonated = ;
-const DateDonated = ;
-const AnyObservations = ;
