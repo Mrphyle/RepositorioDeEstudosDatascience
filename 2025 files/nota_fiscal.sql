624c9951b11eed81d082fa43ff889857ec797fef
Diego Simão Gonçalves <163591079+Mrphyle@users.noreply.github.com>
2025-04-22 18:06:14 -0300

    Refactor Nota_Fiscal table and add new tables for Cliente, cep, and produto

--- 2025 files/nota_fiscal.sql
+++ 2025 files/nota_fiscal.sql
@@ -4,19 +4,10 @@ CREATE TABLE Nota_Fiscal (
     cnpj_cpf VARCHAR(14),
     inscricao_estadual VARCHAR(12),
     endereco VARCHAR(50),
-    bairro VARCHAR(75),
-    cep VARCHAR(8),
-    municipio VARCHAR(50),
-    uf VARCHAR(2),
+    id_cep VARCHAR(8),
     fone_fax VARCHAR(15),
     id_produto VARCHAR(50),
-    descricao_produto_servico VARCHAR(50),
-    ncm_sh VARCHAR(8),
-    csosn VARCHAR(2),
-    cfop VARCHAR(4),
-    un VARCHAR(10),
     qtde DECIMAL(10,2),
-    preco_um DECIMAL(10,2),
     preco_total DECIMAL(10,2),
     desconto DECIMAL(10,2),
     bcip DECIMAL(10,2),
@@ -24,4 +15,29 @@ CREATE TABLE Nota_Fiscal (
     vrl_ipi DECIMAL(10,2),
     icms_percent DECIMAL(5,2),
     ipi_percent DECIMAL(5,2)
+);
+CREATE TABLE Cliente (
+    id_cliente INT,
+    razao_social VARCHAR(50),
+    cnpj_cpf VARCHAR(14),
+    inscricao_estadual VARCHAR(12),
+    endereco VARCHAR(50),
+    id_cep VARCHAR(8),
+    fone_fax VARCHAR(15),
+);
+CREATE TABLE cep (
+    id_cep INT,
+    Cidade VARCHAR(50),
+    logradouro VARCHAR(50),
+    bairro VARCHAR(75),
+    uf VARCHAR(2)
+);
+CREATE TABLE produto (
+    id_produto INT,
+    descricao_produto_servico VARCHAR(50),
+    qtde int,
+    preco_un int,
+    ncm_sh VARCHAR(8),
+    csosn VARCHAR(2),
+    cfop VARCHAR(4),
 );
