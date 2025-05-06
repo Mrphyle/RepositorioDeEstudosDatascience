13b1b5ac93edb6dd4ba1d2b1f2f328bba1aef636
Diego Simão Gonçalves <163591079+Mrphyle@users.noreply.github.com>
2025-05-06 17:20:15 -0300

    Add new Python scripts, SQL files, and CSV data; remove obsolete files

--- 2025 files/dashborads.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-import pandas as pd
-import plotly.express as px
-
-st.set_page_config(layout="wide")
-df = pd.read_csv("fluxo.csv", sep=";", decimal=",")
-df 
---
+++ 2025 files/python/vendaper vendedor/dashvendavendedor.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import openpyxl as px
+sheetinport = r"C:\Users\diego.sgoncalves10\Documents\GitHub\RepositorioDeEstudosDatascience\2025 files\vendaper vendedor\dados-normalizados.xlsx"
+def read_exel_sheets():
+    sheets1 = pd.read_excel(sheetinport, sheet_name="dEstado")
+    sheet2 = pd.read_excel(sheetinport,sheet_name="fMovimentação")
+    sheet3 = pd.read_excel(sheetinport,sheet_name="dVendedor")
+    sheet4 = pd.read_excel(sheetinport,sheet_name="dProduto")
+    return sheets1, sheet2, sheet3, sheet4
+def redrect():
+    dEstado, fMovimentacao, dVendedor, dProduto = read_exel_sheets()
+    CodigoEstado = fMovimentacao['Codig_Estado'].map(dEstado.set_index('Codig_Estado')['Estado'])
+    
+    
